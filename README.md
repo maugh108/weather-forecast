90f324e3825464665b4a07d1a8ebfa663dbf5b4e
maugh108 <maugh108@outlook.com>
2022-10-22 20:13:31 -0500

    repo creado y proyecto arriba

---
+++ README.md
@@ -0,0 +1,31 @@
+# Calendar
+## Challenge Module05  17/Oct/2022
+### Último cambio: Mauricio García Hernández 17/Oct/2022
+
+### User Story:
+COMO UN empleado con una agenda ocupada
+QUIERO agregar eventos importantes en una agenda diaria
+PARA QUE pueda manejar mi tiempo de manera efectiva
+
+
+### Criterios de aceptación:
+CONSIDERANDO que estoy utilizando una agenda diaria para crear un calendario
+CUANDO abro la agenda
+ENTONCES el dia actual se muestra en la parte superior del calendario
+CUANDO me desplazo hacia abajo
+ENTONCES se me presentan bloques de tiempo para el horario laboral estándar
+CUANDO  veo los bloques de tiempo para ese día
+ENTONCES cada bloque de tiempo está codificado por colores para indicar si está en el pasado, presente o futuro
+CUANDO hago clic en un bloque de tiempo
+ENTONCES puedo ingresar un evento
+CUANDO hago clic en el botón de guardar para dicho bloque de tiempo
+ENTONCES el texto para dicho evento se guarda en el almacenamiento local 
+CUANDO recargo la página
+ENTONCES los eventos guardados persisten
+
+
+![Preview](./assets/img/preview.png)
+
+
+* Repositorio: https://github.com/maugh108/calendar
+* URL: https://maugh108.github.io/calendar/
---
+++ assets/js/script.js
@@ -0,0 +1,97 @@
+const APIkey = "25193229136a8f4f79ec1c1878946711";
+var cityNameEl = document.getElementById("city-name");
+var tempEl = document.getElementById("temp");
+var windEl = document.getElementById("wind");
+var humidEl = document.getElementById("humid");
+var cardcontEl = document.getElementById("weather-cards");
+
+function getEmoji(weather) {
+
+    var clearSky = " ☀️";
+    var rain = " 🌧";
+    var clouds = " 🌥";
+
+    if (weather.includes("rain")){
+        return rain;
+    } else if (weather.includes("clear")) {
+        return clearSky;
+    } else if (weather.includes("clouds")) {
+        return clouds;
+    }
+}
+
+function renderData(foreCast) {
+
+    cityNameEl.textContent = foreCast.city.name + getEmoji(foreCast.list[0].weather[0].description);
+    tempEl.textContent = "Temperature: "+ foreCast.list[0].main.temp + "°C";
+    windEl.textContent = "Wind: "+ foreCast.list[0].wind.speed + " m/s";
+    humidEl.textContent = "Humidity: "+ foreCast.list[0].main.humidity + "%";
+
+    for ( var  i = 1; i <= 5; i++) {
+        
+        var today = moment().add(i, 'days').calendar();
+        var dayF = moment(today).format('l');  
+        var day = "day" + i;
+        var pT = "pT" + i;
+        var pW = "pW" + i;
+        var pH = "pH" + i;
+        var em = "em" + i;
+        var dayEl = document.getElementById(day);
+        var pTEl = document.getElementById(pT);
+        var pWEl = document.getElementById(pW);
+        var pHEl = document.getElementById(pH);
+        var emEl = document.getElementById(em);
+
+        dayEl.textContent = moment().add( i, 'days').calendar();
+        emEl.textContent = getEmoji(foreCast.list[i].weather[0].description);      
+        pTEl.textContent = "Temperature: " + foreCast.list[i].main.temp + "°C";
+        pWEl.textContent = "Wind: "+ foreCast.list[i].wind.speed + " m/s";
+        pHEl.textContent = "Humidity: "+ foreCast.list[i].main.humidity + "%";
+    }
+
+    return; 
+
+}
+
+function getForecast(lat, lon) {
+
+    const url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&cnt=6&appid=" + APIkey + "&units=metric";
+    
+    fetch(url).then(function (response) {
+       
+        if (response.ok) {
+            response.json().then(function (data) {
+              renderData(data);
+            });
+          } else {
+          console.log("404");
+        }
+      });
+}
+
+function getData(zipCode, countryCode) {
+
+    let url = "https://api.openweathermap.org/geo/1.0/zip?zip=" + zipCode + "," + countryCode + "&appid=" + APIkey;
+    
+    fetch(url).then(function (response) {
+       
+        if (response.ok) {
+            response.json().then(function (data) {
+                getForecast(data.lat, data.lon)
+            });
+          } else {
+          console.log("404");
+        }
+      });
+
+    
+};
+
+getData("06300", "MX");
+
+
+
+
+
+
+
